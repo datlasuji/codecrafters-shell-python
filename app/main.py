36c083dcbae17a3461c312b7a668d3e77863ff88
dkkv0000 <dkkv0000@gmail.com>
2026-01-11 14:33:44 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
 import sys
 import subprocess
+import os
+
+BUILTINS = ["exit", "type", "echo"]
+
 
 while True:
     # Print prompt
@@ -8,20 +12,49 @@ while True:
 
     line = sys.stdin.readline()
     if not line:
-        break  # EOF
+        break
 
     line = line.strip()
     if not line:
         continue
 
     parts = line.split()
+    command = parts[0]
 
     # exit builtin
-    if parts[0] == "exit":
+    if command == "exit":
         sys.exit(0)
 
+    # type builtin
+    if command == "type":
+        if len(parts) < 2:
+            continue
+
+        target = parts[1]
+
+        if target in BUILTINS:
+            sys.stdout.write(f"{target} is a shell builtin\n")
+            sys.stdout.flush()
+            continue
+
+        found = False
+        for path in os.environ.get("PATH", "").split(":"):
+            full_path = os.path.join(path, target)
+            if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
+                sys.stdout.write(f"{target} is {full_path}\n")
+                sys.stdout.flush()
+                found = True
+                break
+
+        if not found:
+            sys.stdout.write(f"{target}: not found\n")
+            sys.stdout.flush()
+
+        continue
+
+    # external commands
     try:
         subprocess.run(parts)
     except FileNotFoundError:
-        sys.stdout.write(f"{parts[0]}: command not found\n")
+        sys.stdout.write(f"{command}: command not found\n")
         sys.stdout.flush()
