6106d1813d129729edc779b87f3960df99030465
dkkv0000 <dkkv0000@gmail.com>
2026-01-11 13:35:59 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,4 +1,17 @@
 import sys
+import subprocess
 
+# Print prompt
 sys.stdout.write("$ ")
 sys.stdout.flush()
+
+# Read command
+line = sys.stdin.readline().strip()
+
+if line:
+    parts = line.split()
+    try:
+        subprocess.run(parts)
+    except FileNotFoundError:
+        sys.stdout.write(f"{parts[0]}: command not found\n")
+        sys.stdout.flush()
