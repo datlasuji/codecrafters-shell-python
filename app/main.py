7bd8f15eae3f99c4a3e13865b673f97d52fae367
dkkv0000 <dkkv0000@gmail.com>
2026-01-11 13:25:37 +0530

    [hi]

--- app/main.py
+++ app/main.py
@@ -1,11 +1,4 @@
 import sys
 
-
-def main():
-    # TODO: Uncomment the code below to pass the first stage
-    # sys.stdout.write("$ ")
-    pass
-
-
-if __name__ == "__main__":
-    main()
+sys.stdout.write("$ ")
+sys.stdout.flush()
